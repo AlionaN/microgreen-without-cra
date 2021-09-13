d1ff9bd68f0591b84fc8e3784fd03887e4761ff0
anovyk <anovyk@exadel.com>
2021-09-13 17:44:19 +0300

    feat: implement Error 403 component

---
+++ src/components/Error403/Error403.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Main } from '@components/AppLayout';
+import { Button } from '@components/Button';
+
+export const Error404: React.FC = () => {
+  return (
+    <Main>
+      <>
+        <div>403</div>
+        <div>You don`t have permission</div>
+        <Button btnText="Go to home" />
+      </>
+    </Main>
+  );
+};
