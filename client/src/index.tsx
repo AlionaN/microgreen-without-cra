70e60f6fe8d1bbe33c6dd3f6adc4454913116163
anovyk <anovyk@exadel.com>
2021-09-28 10:12:59 +0300

    feat: add geleral styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
-
+import './general.scss';
 
 ReactDOM.render(
   <Provider store={appStore}>
