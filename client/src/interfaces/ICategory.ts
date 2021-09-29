c9379ade3379579d3104417b12db138bbe736c7d
anovyk <anovyk@exadel.com>
2021-09-29 18:21:55 +0300

    fix: change ICategory interface

--- client/src/interfaces/ICategory.ts
+++ client/src/interfaces/ICategory.ts
@@ -1,5 +1,3 @@
 export interface ICategory {
   title: string,
-  _id: string,
-  __v: number,
 }
--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './IPageSettings';
 export * from './ICard';
 export * from './ICategory';
+export * from './ICategoryFromDB';
 export * from './IUser';
 export * from './IStatus';
