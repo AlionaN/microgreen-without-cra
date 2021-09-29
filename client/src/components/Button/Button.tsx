891b75d275e018728d731271a73f6f996bc3c895
anovyk <anovyk@exadel.com>
2021-09-29 18:21:35 +0300

    fix: add type to onClick listener

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ import { MouseEvent } from 'react';
 interface IProps {
   btnText: string | JSX.Element,
   classes?: string,
-  onClick: (e: MouseEvent<HTMLButtonElement | MouseEvent>) => void,
+  onClick: (e: MouseEvent<HTMLButtonElement | JSX.Element | MouseEvent>) => void,
 }
 
 export const Button: React.FC<IProps> = ({ btnText, classes, onClick }: IProps) => {
