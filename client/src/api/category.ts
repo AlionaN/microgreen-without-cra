0daf40de7d7c7048e5cfe321da37c83efa5f418a
anovyk <anovyk@exadel.com>
2021-09-29 18:21:14 +0300

    fix: change api requests for categories

--- client/src/api/category.ts
+++ client/src/api/category.ts
@@ -1,10 +1,49 @@
+import { ICategory } from '@/interfaces';
 import { apiURL } from './apiConfig';
 
-export const getCategories = () => fetch(`${apiURL}/categories`, {
-  method: 'GET'
-});
+export const getCategories = async (): Promise<Response> => {
+  const response = await fetch(`${apiURL}/categories`);
+  const result = await response.json();
+  
+  return result;
+};
 
-export const postCategory = (data: BodyInit) => fetch(`${apiURL}/categories`, {
-  method: 'POST',
-  body: data
-});
+export const postCategory = async (data: ICategory) => {
+  const response = await fetch(`${apiURL}/categories`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(data)
+  });
+
+  const result = await response.json();
+  console.log(result);
+  
+  return result;
+};
+
+export const deleteCategory = async (id: string) => {
+  const response = await fetch(`${apiURL}/categories/${id}`, {
+    method: 'DELETE',
+  });
+
+  const result = await response.json();
+  console.log(result);
+
+  return result;
+};
+
+export const editCategory = async(id: string, data: ICategory) => {
+  const response = await fetch(`${apiURL}/categories/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+
+  return result;
+};
