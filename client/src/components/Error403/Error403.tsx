f97192be98b8b5af7de3dbb12c52419fbced2a0b
anovyk <anovyk@exadel.com>
2021-10-04 21:02:41 +0300

    feat: add click handler to Go home button

--- client/src/components/Error403/Error403.tsx
+++ client/src/components/Error403/Error403.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Main } from '@/components/AppLayout';
 import { Button } from '@/components/Button';
+import { useHistory } from 'react-router';
+import { Routes } from '@/enums';
 
-export const Error404: React.FC = () => {
+export const Error403: React.FC = () => {
+  const history = useHistory();
+
+  const onGoHomeClick = () => {
+    history.push(Routes.Root);
+  };
+
   return (
     <Main>
       <>
         <div>403</div>
         <div>You don`t have permission</div>
-        <Button btnText="Go to home" />
+        <Button btnText="Go to home" onClick={onGoHomeClick} />
       </>
     </Main>
   );
--- client/src/components/Error404/Error404.tsx
+++ client/src/components/Error404/Error404.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Main } from '@/components/AppLayout';
 import { Button } from '@/components/Button';
+import { useHistory } from 'react-router';
+import { Routes } from '@/enums';
 
 export const Error404: React.FC = () => {
+  const history = useHistory();
+
+  const onGoHomeClick = () => {
+    history.push(Routes.Root);
+  };
+
   return (
     <Main>
       <>
         <div>404</div>
         <div>Page not found</div>
-        <Button btnText="Go to home" />
+        <Button btnText="Go to home" onClick={onGoHomeClick} />
       </>
     </Main>
   );
