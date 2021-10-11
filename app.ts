10c8d5557cf8ecb92a088152be286528fd22f438
anovyk <anovyk@exadel.com>
2021-10-11 11:36:56 +0300

    feat: increase limit for requests

--- app.ts
+++ app.ts
@@ -14,7 +14,8 @@ const PORT: number = +process.env.PORT || 5000;
 
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-app.use(express.json());
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
 
 app.use('/api/auth', AuthRoutes);
 app.use('/api/categories', CategoryRoutes);
