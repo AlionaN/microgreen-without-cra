1820d279f0fda57041f7e8c3263e6c28245626b4
anovyk <anovyk@exadel.com>
2021-10-20 00:44:04 +0300

    feat: add role to mongo model User, remove isSignIn field and make img field unrequired

--- server/models/User.ts
+++ server/models/User.ts
@@ -1,28 +1,33 @@
 import { Schema, model, Types } from 'mongoose';
 import { IProduct } from './Product';
 
+enum UserRoles {
+  admin = 'ADMIN',
+  user = 'USER'
+};
+
 interface IUser {
   firstName: string,
   secondName: string,
   email: string,
-  img: string,
+  img?: string,
   password: string,
   password_confirm: string,
-  isSignIn: boolean,
   cart: IProduct[],
   favourites: IProduct[],
-}
+  role: UserRoles,
+};
 
 const schema = new Schema<IUser>({
   firstName: {  type: String, required: true },
   secondName: {  type: String, required: true },
   email: {  type: String, required: true,  unique: true },
-  img: {  type: String, required: true },
+  img: {  type: String, required: false },
   password: {  type: String, required: true },
   password_confirm: {  type: String, required: true },
-  isSignIn: {  type: Boolean, required: true },
   cart: [{ type: Types.ObjectId, ref: 'Product' }],
-  favourites: [{ type: Types.ObjectId, ref: 'Product' }]
+  favourites: [{ type: Types.ObjectId, ref: 'Product' }],
+  role: [{ type: String, required: true }]
 });
 
 const UserModel = model<IUser>('User', schema);
