ac22986b33239e1be7810d69162bdc41c997a750
anovyk <anovyk@exadel.com>
2021-10-20 00:54:34 +0300

    feat: add redux devtools

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import { rootReducer } from './reducers';
 import { rootSaga } from './rootSaga';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const appStore = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware),
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(rootSaga);
