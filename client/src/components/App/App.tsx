42e06d5a5991e999b77ee24279b54eab2f230852
anovyk <anovyk@exadel.com>
2021-10-20 00:48:15 +0300

    feat: add localStorage check for token being

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { AppRouter } from '@/features/AppRouter';
 import './App.module.scss';
+import { useDispatch } from 'react-redux';
+import * as actions from '@/store/actions';
 
 export const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem('token');
+  
+  if (token) {
+    dispatch(actions.loginSuccess())
+  }
+
   return (
     <AppRouter />
   );
