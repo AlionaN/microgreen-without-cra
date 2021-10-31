98eab790b5c93930adce7a22622943c1912b235f
anovyk <anovyk@exadel.com>
2021-11-01 00:38:03 +0200

    feat: add redux flow functions for cart and guest cart to root files

--- client/src/store/reducers/index.ts
+++ client/src/store/reducers/index.ts
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import { userReducer } from './user';
 import { categoryReducer } from './category';
 import { productReducer } from './product';
+import { cartReducer } from './cart';
+import { guestCartReducer } from './guestCart';
 
 export const rootReducer = combineReducers({
   userReducer,
   categoryReducer,
-  productReducer
+  productReducer,
+  cartReducer,
+  guestCartReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- client/src/store/rootSaga.ts
+++ client/src/store/rootSaga.ts
@@ -3,7 +3,8 @@ import { all, fork } from 'redux-saga/effects';
 import categorySaga from '@/store/sagas/category';
 import productSaga from '@/store/sagas/product';
 import userSaga from '@/store/sagas/user';
+import cartSaga from '@/store/sagas/cart';
 
 export function* rootSaga() {
-  yield all([fork(categorySaga), fork(productSaga), fork(userSaga)]);
+  yield all([fork(categorySaga), fork(productSaga), fork(userSaga), fork(cartSaga)]);
 }
