94fe6239f5e415f6de9abfeb851d22686441b253
anovyk <anovyk@exadel.com>
2021-11-01 00:22:29 +0200

    feat: add Product page

---
+++ client/src/pages/Internal/Product/Product.tsx
@@ -0,0 +1,63 @@
+import { RootState } from '@/store/reducers';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from './Product.module.scss';
+import * as actions from '@/store/actions';
+import { ICart, IProductFromDB } from '@/interfaces';
+import { Button } from '@/components/Button';
+import { useParams } from 'react-router';
+import { AppLayout } from '@/components/AppLayout';
+import { SITE_DOMAIN } from '@/constants';
+
+interface urlParams {
+  productId: string
+};
+
+export const Product: React.FC = () => {
+  const { productId } = useParams<urlParams>();
+  const dispatch = useDispatch();
+  const userId = localStorage.getItem('userId');
+  const user = useSelector((state: RootState) => state.userReducer.user);
+  const product: IProductFromDB = useSelector((state: RootState) => state.productReducer.product);
+  const [productQuantity, setProductQuantity] = useState(1);
+  const cart: ICart = useSelector((state: RootState) => state.cartReducer.cart);
+
+  useEffect(() => {
+    dispatch(actions.getProduct(productId));
+    userId !== null && dispatch(actions.getUser(userId));
+    user && dispatch(actions.getCart(user.cart));
+
+    if (!cart){ 
+      dispatch(actions.getGuestCart());
+    };
+  }, []);
+
+  const onQuantityChange = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setProductQuantity(+target.value);
+  };
+
+  const onAddToCart = () => {
+    if (cart) {
+      dispatch(actions.addProductToCart(cart._id, productId, productQuantity));
+    } else {
+      dispatch(actions.addProductToGuestCart(product, productQuantity));
+    }
+  };
+
+  return (
+    <AppLayout>
+      <div className={styles.product}>
+        <img className={styles.productImage} src={`${SITE_DOMAIN}/${product?.image}`} alt={product?.title} />
+        <div className={styles.productInfo}>
+          <h2 className={styles.productTitle}>{product?.title}</h2>
+          <p className={styles.productDescr}>{product?.description}</p>
+          {product?.size && <div className={styles.productSize}>Size: {product?.size}</div>}
+          {product?.amount && <div className={styles.productAmount}>Size: {product.amount}</div>}
+          <input type="number" className={styles.productQuantity} value={productQuantity} onChange={(e) => onQuantityChange(e)} />
+          <Button btnText="Add to cart" onClick={onAddToCart} />
+        </div>
+      </div>
+    </AppLayout>
+  );
+};
