2f2acc69f1a16628c3f4eb483fea81afc6e1f34c
anovyk <anovyk@exadel.com>
2021-11-01 00:39:08 +0200

    fix: add user and cart checking

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppRouter } from '@/features/AppRouter';
 import './App.module.scss';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as actions from '@/store/actions';
+import { RootState } from '@/store/reducers';
 
 export const App: React.FC = () => {
   const dispatch = useDispatch();
   const token = localStorage.getItem('token');
+  const userId = localStorage.getItem('userId');
+  const user = useSelector((state: RootState) => state.userReducer.user);
   
-  if (token) {
-    dispatch(actions.loginSuccess())
+  useEffect(() => {
+    userId && userId !== null && dispatch(actions.getUser(userId));
+  }, []);
+
+  useEffect(() => {
+    if (user && user !== null) { 
+      dispatch(actions.getCart(user.cart));
+    } else if (!localStorage.getItem('cart')){
+      localStorage.setItem('cart', JSON.stringify({
+        items: [],
+        totalPrice: 0,
+        totalQuantity: 0
+      }));
+      dispatch(actions.getGuestCart());
+    }
+  }, []);
+  
+  if (token && userId) {
+    dispatch(actions.loginSuccess());
+    localStorage.removeItem('cart');
   }
 
   return (
