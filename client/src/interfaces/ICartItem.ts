002279bc8862de56de2b907d85da4ff51588d085
anovyk <anovyk@exadel.com>
2021-11-01 00:27:03 +0200

    feat: implement cart interfaces

---
+++ client/src/interfaces/ICartItem.ts
@@ -0,0 +1,24 @@
+import { IProductFromDB } from ".";
+
+export interface ICart {
+  _id: string,
+  items: ICartItem[],
+  totalQuantity: number,
+  totalPrice: number,
+  createdAt: string,
+  updatedAt: string,
+  __v: number
+};
+
+export interface ICartItem {
+  product: IProductFromDB,
+  itemPrice: number,
+  quantity: number,
+  _id: string
+};
+
+export interface IGuestCart {
+  items: ICartItem[],
+  totalQuantity: number,
+  totalPrice: number
+};
