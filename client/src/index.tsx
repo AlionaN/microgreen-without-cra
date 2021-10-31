973f5e8ece1d2d196691360ec0e97ddd15d35ee0
anovyk <anovyk@exadel.com>
2021-11-01 00:09:07 +0200

    refactor: add aliases to imports

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import appStore from './store/store';
+import appStore from '@/store/store';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './components/App';
-import './general.scss';
+import { App } from '@/components/App';
+import '@/general.scss';
 
 ReactDOM.render(
+  <>
   <Provider store={appStore}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </BrowserRouter>
-  </Provider>,
+  </Provider>
+  <script src="http://localhost:8097"></script>
+  </>,
   document.getElementById('root'),
 );
