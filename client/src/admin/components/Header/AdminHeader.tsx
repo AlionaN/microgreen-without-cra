b9f8dad4908274119f6cc43d1bf1cdbf26616fb7
anovyk <anovyk@exadel.com>
2021-11-08 23:10:27 +0200

    feat: add link to dashboard

--- client/src/admin/components/Header/AdminHeader.tsx
+++ client/src/admin/components/Header/AdminHeader.tsx
@@ -9,6 +9,7 @@ export const AdminHeader: React.FC = () => {
       <div className={styles.headerTitle}>Microgreen Admin Panel</div>
       <nav className={styles.headerNav}>
         <ul className={styles.headerNavList}>
+          <li className={styles.headerNavListItem}><span className={styles.headerNavListItemText}><NavLink to={AdminRoutes.Admin}>Dashboard</NavLink></span></li>
           <li className={styles.headerNavListItem}><span className={styles.headerNavListItemText}>Shop</span>
             <ul className={styles.headerNavSublist}>
               <li className={styles.headerNavSublistItem}><NavLink to={AdminRoutes.Categories}>Categories</NavLink></li>
