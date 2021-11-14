418e738957db6fbe3ec00fd8e6c15d24fb258c07
anovyk <anovyk@exadel.com>
2021-11-15 01:58:53 +0200

    feat: add hamburger menu and mobile menu animation

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Routes } from '@/enums';
 import { Logo } from '@/components/Logo';
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store/reducers';
 import * as actions from '@/store/actions';
 import { Cart } from '@/components/Cart';
+import { HiMenu } from 'react-icons/hi';
+import { IoClose } from 'react-icons/io5';
 
 interface IHeader {
   onSignInClick?: () => void,
@@ -17,6 +19,7 @@ export const Header: React.FC<IHeader> = ({ onSignInClick }: IHeader) => {
   const isSignIn = useSelector((state: RootState) => state.userReducer.isLogIn);
   const user = useSelector((state: RootState) => state.userReducer.user);
   const userId = String(localStorage.getItem('userId'));
+  const [isMobileMenuShown, setIsMobileMenuShown] = useState<boolean>(false);
 
   useEffect(() => {
     userId && userId !== null && userId !== 'null' && dispatch(actions.getUser(userId));
@@ -27,10 +30,23 @@ export const Header: React.FC<IHeader> = ({ onSignInClick }: IHeader) => {
     dispatch(actions.removeCart());
   };
 
+  const onHamburgerClick = () => {
+    isMobileMenuShown ? setIsMobileMenuShown(false) : setIsMobileMenuShown(true);
+  };
+
   return (
     <header className={styles.header}>
+      <div className={styles.headerTop}>
+        <div className={styles.leftPart}>Place an order for 500 UAH and get free shipping</div>
+        <div className={styles.centerPart}>All seeds are organic certified and produced in EU cultivation</div>
+      </div>
       <div className={styles.headerInner}>
         <Logo />
+        <div className={`${styles.hamburger} ${isMobileMenuShown ? styles.active : ''}`} onClick={onHamburgerClick}>
+          {isMobileMenuShown
+            ? <IoClose />
+            : <HiMenu />}
+        </div>
         <nav className={styles.nav}>
           <ul className={styles.navList}>
             <li className={styles.navListItem}>
@@ -55,7 +71,7 @@ export const Header: React.FC<IHeader> = ({ onSignInClick }: IHeader) => {
             ? <>
               <div className={styles.userFuncAuth}>
                 <div>Hello, {user.firstName}</div>
-                <div onClick={onSignOutClick}>Sign out</div>
+                <div onClick={onSignOutClick} className={styles.userFuncSignOut} >Sign out</div>
               </div>
               {user.img && <div className={styles.userImg}><img src={user.img}/></div>}
               </>
