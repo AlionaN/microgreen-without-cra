26bda777e7baa45d3c626bc9b1990b1766d2963e
anovyk <anovyk@exadel.com>
2021-11-15 01:55:26 +0200

    feat: add hamburger menu on mobile screen and rewrite component with materialUI grid

--- client/src/components/AppLayout/AdminLayout.tsx
+++ client/src/components/AppLayout/AdminLayout.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { AdminHeader } from '@/admin/components/Header';
 import { AdminFooter } from '@/admin/components/Footer';
-import styles from './AppLayout.module.scss';
+import styles from './AdminLayout.module.scss';
+import { Grid } from '@mui/material';
+import { HiMenu } from 'react-icons/hi';
+import { IoClose } from 'react-icons/io5';
+import { useState } from 'react';
 
 export const AdminLayout: React.FC = ({ children }) => {
+  const [isMobileMenuShown, setIsMobileMenuShown] = useState<boolean>(false);
+
+  const onHamburgerClick = () => {
+    isMobileMenuShown ? setIsMobileMenuShown(false) : setIsMobileMenuShown(true);
+  };
 
   return (
     <div className={`${styles.container} ${styles.admin}`}>
-      <AdminHeader />
-        <main className={styles.main}>
+      <AdminHeader isMobileShown={isMobileMenuShown} />
+      <div className={`${styles.hamburger} ${isMobileMenuShown ? styles.active : ''}`} onClick={onHamburgerClick}>
+        {isMobileMenuShown
+          ? <IoClose className={styles.closeIcon} />
+          : <HiMenu />}
+      </div>
+      <main className={styles.main}>
+        <Grid container spacing={2} columns={{ xs: 12, md: 12 }}>
           {children}
-        </main>
+        </Grid>
+      </main>
       <AdminFooter />
     </div>
   );
