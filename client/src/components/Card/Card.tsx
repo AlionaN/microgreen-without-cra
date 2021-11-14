4d1697b00c6b18002175bab54c849ea2cc102bb0
anovyk <anovyk@exadel.com>
2021-11-15 01:59:58 +0200

    feat: add description field

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -16,7 +16,7 @@ export const Card: React.FC<IProps> = ({ card }: IProps) => {
   const dispatch = useDispatch();
   const guestCart = useSelector((state: RootState) => state.guestCartReducer.guestCart);
   const cart = useSelector((state: RootState) => state.cartReducer.cart);
-  const { image, title, price, _id } = card;
+  const { image, title, description, price, _id } = card;
 
   const onAddToCartClick = (e: MouseEvent<HTMLButtonElement | JSX.Element | MouseEvent>) => {
     e.stopPropagation();
@@ -28,18 +28,21 @@ export const Card: React.FC<IProps> = ({ card }: IProps) => {
   };
 
   return (
-    <Link to={`${Routes.Shop}/${card._id}`}>
-      <li className={styles.card}>
+    <div className={styles.card}>
+      <Link to={`${Routes.Shop}/${card._id}`} className={styles.cardLink}>
         <img src={image} className={styles.cardImg} alt={title} />
         <div className={styles.cardInfo}>
           <div className={styles.cardInfoTitle}>{title}</div>
-          <div className={styles.cardInfoPrice}>
+          <div className={styles.cardInfoDescription}>{description}</div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.cardPrice}>
             {price.toFixed(2)}
             $
           </div>
+          <Button btnText="Add to cart" onClick={(e) => onAddToCartClick(e)} />
         </div>
-        <Button btnText="Add to cart" onClick={(e) => onAddToCartClick(e)} />
-      </li>
-    </Link>
+      </Link>
+    </div>
   );
 }
